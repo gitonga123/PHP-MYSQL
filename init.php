29443f0dc4141890d77b36896c38593deaed33fe
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-05-08 12:39:57 +0300

    File Arrangement

--- init.php
+++ init.php
@@ -2,22 +2,22 @@
 use Bookstore\Domain\Book;
 use Bookstore\Domain\Customer;
 
-function __autoloader($classname){
-	$lastSlash = strpos($classname, '\\') + 1;
-	$classname = substr($classname, $lastSlash);
-	$directory = str_replace('\\','/', $classname);
-	$filename = __DIR__.'/src/'.$directory. '.php';
+function __autoloader($classname)
+{
+    $lastSlash = strpos($classname, '\\') + 1;
+    $classname = substr($classname, $lastSlash);
+    $directory = str_replace('\\','/', $classname);
+    $filename = __DIR__.'/src/'.$directory. '.php';
 
-	require_once($filename);
+    require_once($filename);
 }
 
-spl_autoload_register('autoloader')
-;
+spl_autoload_register('autoloader');
+
 $book1 = new Book("1984", "George Orwell", 9785267006323, 12);
-$book2 = new Book("To Kill a Mockingbird", "Harper Lee",
-9780061120084, 2);
+$book2 = new Book("To Kill a Mockingbird", "Harper Lee", 9780061120084, 2);
 $customer1 = new Customer(3, 'John', 'Doe', 'johndoe@mail.com');
-$customer2 = new Customer(1,'Mary', 'Poppins', 'mp@mail.com');
+$customer2 = new Customer(1, 'Mary', 'Poppins', 'mp@mail.com');
 $customer3 = new Customer(7, 'James', 'Bond', '007@mail.com');
 
 $customer1->setFirstname("James");
