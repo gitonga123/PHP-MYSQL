efd27a2fd0d756d66d094e07c8938cb8fc03ff6c
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-10-04 07:55:30 +0300

    OOP IN PHP first commit

--- init.php
+++ init.php
@@ -1,27 +1,27 @@
 <?php
-/**
- * Index FIle
- */
-
-use Bookstore\Domain\Book;
-use Bookstore\Domain\Customer;
-
-
-function __autoloader($classname)
-{
-    $lastSlash = strpos($classname, '\\') + 1;
-    $classname = substr($classname, $lastSlash);
-    $directory = str_replace('\\','/', $classname);
-    $filename = __DIR__.'/src/Domain/'.$directory. '.php';
-
-    require_once($filename);
-}
-//spl_autoload_register('autoloader');
-$book1 = new Book("1984", "George Orwell", 9785267006323, 12);
-$customer1 = new Customer(3, 'John', 'Doe', 'johndoe@mail.com');
-
-
-$customer1->setFirstname("James");
-
-var_dump($customer1::getLastId());
-var_dump(Customer::getLastId());
+///**
+// * Index FIle
+// */
+//
+//use Bookstore\Domain\Book;
+//use Bookstore\Domain\Customer;
+//
+//
+//function __autoloader($classname)
+//{
+//    $lastSlash = strpos($classname, '\\') + 1;
+//    $classname = substr($classname, $lastSlash);
+//    $directory = str_replace('\\','/', $classname);
+//    $filename = __DIR__.'/src/Domain/'.$directory. '.php';
+//
+//    require_once($filename);
+//}
+////spl_autoload_register('autoloader');
+//$book1 = new Book("1984", "George Orwell", 9785267006323, 12);
+//$customer1 = new Customer(3, 'John', 'Doe', 'johndoe@mail.com');
+//
+//
+//$customer1->setFirstname("James");
+//
+//var_dump($customer1::getLastId());
+//var_dump(Customer::getLastId());
