e6df6ab9cded0788be449945cb3c26ad70c7c817
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-01 08:48:34 +0300

    Fizzy Buzz

--- javascript/101/fizz_buzz.js
+++ javascript/101/fizz_buzz.js
@@ -1,5 +1,16 @@
-var counter = 1;
-while (counter >= 100) {
-    console.log(counter);
-    counter++;
+let counter = 100;
+
+while (counter >= 1) {
+    if (counter % 3 ===  0)
+        console.log('Fizz');
+
+    else if (counter % 5 === 0)
+        console.log("Buzz");
+
+    else if ((counter % 3 === 0) && (counter % 5 === 0))
+        console.log("FizzBuzz");
+    else
+        console.log(counter);
+
+    counter--;
 }
