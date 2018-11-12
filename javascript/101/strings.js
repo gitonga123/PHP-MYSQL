a3add6767b5b367a23d21895513bc559cc4748e5
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-12 22:21:21 +0300

    string object' '

--- javascript/101/strings.js
+++ javascript/101/strings.js
@@ -14,3 +14,13 @@ console.log(mystring.charCodeAt(6));
 // codePointAt method returns a non-negative integer that is the
 // Unicode point value
 console.log(mystring.codePointAt(6));
+// the indexof method returns the index within the calling
+// string object of the first occurrence of the specified values,
+// starting  the search at fromIndex.
+// case sensitive
+var searchTearm = 'KEN';
+var indexOfFirst = mystring.indexOf(searchTearm, 5);
+console.log(indexOfFirst);
+// returns the index within the calling String object of the
+// last occurrence of the specified values, searches backwards
+console.log(mystring.lastIndexOf(searchTearm));
