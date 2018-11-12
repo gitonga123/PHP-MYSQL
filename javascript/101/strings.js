47ec7c5b6fbee6f4edd5ede2ce975e722daace4d
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-12 22:03:16 +0300

    string object' '

--- javascript/101/strings.js
+++ javascript/101/strings.js
@@ -1,7 +1,13 @@
 var s = new String('foo');
 console.log(s); // {'0': 'f', '1': 'o'}
 typeof s; //returns object
-var mysting = 'HELLO KENYA';
+var mystring = 'HELLO KENYA';
 var x = mystring.length;
 mystring[0] = 'N'; // thi has no effects since strings are immutable.
 console.log(mystring[0]);
+// returns a new string consiting of the string 
+// located at the specified offset into the string
+console.log(mystring.charAt(6));
+// this methods returns an integer between 0 and 65536  
+// representing the string at the given index
+console.log(mystring.charCodeAt(6));
