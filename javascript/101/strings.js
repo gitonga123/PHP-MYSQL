90edcaf2a46cce5102743b16be4640f37f5d3be0
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2018-11-12 21:29:04 +0300

    string object' '

--- javascript/101/strings.js
+++ javascript/101/strings.js
@@ -1,3 +1,7 @@
 var s = new String('foo');
 console.log(s); // {'0': 'f', '1': 'o'}
 typeof s; //returns object
+var mysting = 'HELLO KENYA';
+var x = mystring.length;
+mystring[0] = 'N'; // thi has no effects since strings are immutable.
+console.log(mystring[0]);
