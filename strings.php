a29156755999511dce08a035b9da61ac355707c9
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-30 14:43:04 +0300

    Strings Manipulation :simple-smile:

---
+++ strings.php
@@ -0,0 +1,26 @@
+<?php
+
+$greetings = "Hellow World";
+echo "\n";
+// this functions returns the length of a string
+echo strlen($greetings);
+
+// this function counts the number of words in a string
+
+echo str_word_count($greetings);
+
+// reverse a string
+
+echo strrev($greetings);
+
+// searching for a specific text within a String
+
+echo strpos("Hello World!", "World");
+
+// Replace text within a string
+
+echo str_replace("World", "Dolly", "Hello World!");
+
+$str = addcslashes($greetings, "d");
+
+echo $str;
