15e7c019042547311151d503c2ee753cc957f2e2
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-30 16:19:31 +0300

    Test Driven Development :sparkles:

---
+++ testing/tests/unit/SampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Sample test
+ */
+class SampleTest extends TestCase
+{
+    public function testTrueAssertsToTrue()
+    {
+        $this->assertFalse(false);
+    }
+}
