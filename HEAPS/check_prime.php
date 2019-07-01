e7ab2a8236ae81734d18453d1deda3f024b3af09
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-07-01 10:27:56 +0300

    Check is prime

--- HEAPS/check_prime.php
+++ HEAPS/check_prime.php
@@ -2,18 +2,16 @@
 
 function is_prime(int $n): bool
 {
-    return is_numeric(sqrt($n));
     if ($n <= 1) {
         return false;
     } else {
-        for ($i = 2; $i < 9; $i++) {
-            if ($i < $n && $n % $i === 0 && s_int(sqrt($n)) == true) {
-                return false;
-            }
+        for ($i = 2; $i <= sqrt($n); $i++) {
+          if ($n % $i === 0) {
+            return false;
+          }
         }
-        return true;
+      return true;
     }
 }
 
 var_dump(is_prime(11282881));
-// 11282881
